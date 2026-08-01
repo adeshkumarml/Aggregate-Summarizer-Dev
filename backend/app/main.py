5036029ca27771a95276c53156e45a0170680d51
adeshkumarml <adesh.ks2002@gmail.com>
2026-08-01 15:36:53 +0530

    Removed temporary code

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,33 +10,13 @@ from app.api.routes.export import router as export_router
 import os
 origins = os.getenv("CORS_ALLOW_ORIGINS", "").split(",")
 
-# temporary
-import time
-import asyncio
-
-async def heartbeat():
-    counter = 0
-    while True:
-        counter += 1
-        print(f"[HB {counter}] {time.strftime('%H:%M:%S')}")
-        await asyncio.sleep(1)
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     redis_okay = await redis_client.health_check()
     if not redis_okay:
         raise RuntimeError("Failed to connect to Redis.")
     print("Redis connected successfully!")
-
-    # temporary
-    heartbeat_task = asyncio.create_task(heartbeat())
-
     yield
-
-    # temporary
-    heartbeat_task.cancel
-
     await redis_client.close()
     print("Redis connection closed!")
 
